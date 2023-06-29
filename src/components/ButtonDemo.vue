e0f130219134a77ae78221df2019946b6750b7f6
jxc <you@example.com>
2023-06-29 14:38:31 +0800

    创建button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,14 @@
 <template>
+    <div>Button示例</div>
+    <h1>示例1</h1>
     <div>
-       button
+       <Button>示例1按钮</Button>
     </div>
 </template>
+
+<script lang="ts">
+import Button from "../lib/Button.vue"
+export default {
+    components: { Button },
+}
+</script>
---
+++ src/lib/Button.vue
@@ -0,0 +1,6 @@
+<template>
+    <button>
+        <slot>默认按钮</slot>
+    </button>
+
+</template>

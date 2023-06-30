ad471ab9f2d787b7df3185cbac608925f5eb2074
jxc <you@example.com>
2023-06-30 10:35:46 +0800

    实现dialog的visiable属性

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,10 +1,15 @@
 <template>
-    <div>
-       <Dialog></Dialog>
+    <div>Dialog示例
+        <h1>示例1</h1>
+        <Button @click="visiable=!visiable">toggle</Button>
+       <Dialog :visiable="visiable" ></Dialog>
     </div>
 </template>
 
 <script setup lang="ts">
-
+import {ref} from 'vue'
 import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
+const visiable = ref(false)
+
 </script>

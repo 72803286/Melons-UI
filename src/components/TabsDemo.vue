c209ddb3a2b3706441a59538ef96e90d3a604c50
jxc <you@example.com>
2023-06-30 16:35:01 +0800

    初始化Tabs,Tab组件

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,5 +1,13 @@
 <template>
-    <div>
-       tabs
-    </div>
+    <div>Tabs示例</div>
+    <h1>示例1</h1>
+    <Tabs>
+        <Tab title="导航1">内容1</Tab>
+        <Tab title="导航2">内容2</Tab>
+    </Tabs>
 </template>
+
+<script setup lang="ts">
+import Tab from '../lib/Tab.vue'
+import Tabs from '../lib/Tabs.vue'
+</script>

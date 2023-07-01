5ff6be3eae8d87f7d1d6edb5afb6c52fc57c64b1
jxc <you@example.com>
2023-07-01 10:23:05 +0800

    完成Tabs子标签必须是tab的验证

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,10 +1,18 @@
 <template>
     <div>
-        <slot></slot>
+        <component v-for="item in defaults " :is="item"></component>
     </div>
 </template>
 
 <script setup lang="ts">
-
-
+import Tab from '../lib/Tab.vue'
+import { useSlots } from 'vue';
+    const slots = useSlots()
+    const defaults = slots.default?.()
+    defaults?.forEach(child => {
+        if(child.type !== Tab){
+            throw new Error('Tabs子标签必须是Tab')
+        }}      
+    );
+    
 </script>

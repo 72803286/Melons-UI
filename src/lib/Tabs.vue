3faa53fe98542482613261d76914093b11d4e94d
jxc <you@example.com>
2023-07-03 13:02:22 +0800

    优化2

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,49 +1,50 @@
 <template>
-    <div class="gulu-tabs">
-        <div class="gulu-tabs-nav" ref = 'container'>
-            <div class="gulu-tabs-nav-item" 
-            v-for="(t, index) in titles" :key="index" :ref="(el) =>{if(t === props.selected) selectedItem=el}" @click="selecte(t)" :class="{selected : (t === props.selected)}" > {{ t }} </div>
-            <div class="gulu-tabs-nav-indicator" ref = 'indicator'></div>
-        </div>
-        <div class="gulu-tabs-content" > 
-            <component class="gulu-tabs-content-item" v-for="(item, index) in defaults " :key="index" :is="item" v-show="item.props?.title === props.selected "></component>
-        </div>
+  <div class="gulu-tabs">
+    <div class="gulu-tabs-nav" ref='container'>
+      <div class="gulu-tabs-nav-item" v-for="(t, index) in titles" :key="index"
+        :ref="(el) => { if (t === props.selected) selectedItem = el }" @click="selecte(t)"
+        :class="{ selected: (t === props.selected) }"> {{ t }} </div>
+      <div class="gulu-tabs-nav-indicator" ref='indicator'></div>
     </div>
+    <div class="gulu-tabs-content">
+      <component class="gulu-tabs-content-item" v-for="(item, index) in defaults " :key="index" :is="item"
+        v-show="item.props?.title === props.selected"></component>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import Tab from '../lib/Tab.vue'
-import { useSlots,ref,onMounted,onUpdated } from 'vue';
+import { useSlots, ref, watchEffect } from 'vue';
 const slots = useSlots()
 const defaults = slots.default?.()
 const selectedItem = ref<any>(null)
 const indicator = ref<any>(null)
 const container = ref<any>(null)
 defaults?.forEach(child => {
-    if (child.type !== Tab) {
-        throw new Error('Tabs子标签必须是Tab')
-    }
+  if (child.type !== Tab) {
+    throw new Error('Tabs子标签必须是Tab')
+  }
 }
 );
 const titles = defaults?.map(tags => tags.props?.title)
 const props = defineProps({
-    selected:String
+  selected: String
 })
 
 const emit = defineEmits(["update:selected"])
-const selecte = (title:String) =>{
-    emit("update:selected",title)
+const selecte = (title: String) => {
+  emit("update:selected", title)
 }
-const x = ()=>{
+
+watchEffect(() => {
   const { width } = selectedItem.value.getBoundingClientRect()
-  indicator.value.style.width = width +'px'
-  const {left:navleft} = container.value.getBoundingClientRect()
-  const {left:navitemsleft} = selectedItem.value.getBoundingClientRect()
-  const left = navitemsleft-navleft
-  indicator.value.style.left = left +'px'
-}
-onMounted(x),
-onUpdated(x)
+  indicator.value.style.width = width + 'px'
+  const { left: navleft } = container.value.getBoundingClientRect()
+  const { left: navitemsleft } = selectedItem.value.getBoundingClientRect()
+  const left = navitemsleft - navleft
+  indicator.value.style.left = left + 'px'
+},{flush:'post'})
 
 
 </script>

25984cbe583ee21a543f772fac8e2f50ca995d12
jxc <you@example.com>
2023-07-03 14:27:46 +0800

    背景渐变

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
     import Topnav from '../components/Topnav.vue'
 </script>
 <template>
-    <div>
+    <div class="topnavAndBanner">
         <Topnav/>
         <div class="banner">
             <h1>Melone UI</h1>
@@ -17,17 +17,22 @@
 
 
 <style lang="scss" scoped>
+.topnavAndBanner {
+  background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
+}
+
 .banner {
   padding: 100px 0;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  background: lightgreen;
-  > .actions{
+
+  >.actions {
     padding: 8px 0;
+
     a {
-      margin:0 8px;
+      margin: 0 8px;
       background: #fff;
       display: inline-block;
       $h: 28px;

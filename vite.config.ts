3207a3764a82860b2acb930641080c1cf3a0a31b
jxc <you@example.com>
2023-07-05 15:30:20 +0800

    @ts-nocheck

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Markdown from 'vite-plugin-md'
@@ -14,9 +15,9 @@ const vueDemoPlugin = {
 		//异步读取文件内容，并转为string类型
 		const file = fs.readFileSync(filePath).toString();
 		//将读取到的文件中的自定义快渲染为AST
-		const parsed = baseParse(file).children.find((n) => (n as any).tag === "demo");
+		const parsed = baseParse(file).children.find((n) => n.tag === "demo");
 		//读取自定义模块中的文本内容
-		const title = (parsed as any).children[0].content;
+		const title = parsed.children[0].content;
 		//将读取文件中的自定义块切分，并转为字符串类型
 		const main = file.split(parsed.loc.source).join("").trim();
 		//以JSON数据类型返回
@@ -27,7 +28,7 @@ const vueDemoPlugin = {
 	},
 };
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [
     vue({

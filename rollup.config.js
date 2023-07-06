f2a5e3408e2e3041bbc873d8da52e9e059f8eed9
jxc <you@example.com>
2023-07-06 12:03:01 +0800

    因为报错重新配置config

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
 // 请先安装 rollup-plugin-esbuild rollup-plugin-vue rollup-plugin-scss sass rollup-plugin-terser
 // 为了保证版本一致，请复制我的 package.json 到你的项目，并把 name 改成你的库名
-import esbuild from 'rollup-plugin-esbuild'
-import vue from 'rollup-plugin-vue'
-import scss from 'rollup-plugin-scss'
-import dartSass from 'sass';
-import { terser } from "rollup-plugin-terser"
+import esbuild from 'rollup-plugin-esbuild'  //把typescript变成js
+import vue from 'rollup-plugin-vue'        //把.vue文件变成js
+import scss from 'rollup-plugin-scss'        //把.scss文件变成js
+import dartSass from 'sass';                  //用于支持rollup-plugin-scss插件
+import { terser } from "rollup-plugin-terser"  //用于把js代码变得看不懂，变丑
 
 export default {
   input: 'src/lib/index.ts',
@@ -12,7 +12,7 @@ export default {
     globals: {
       vue: 'Vue'
     },
-    name: 'Melons-UI',
+    name: 'Melons',
     file: 'dist/lib/gulu.js',
     format: 'umd',
     plugins: [terser()]
@@ -20,9 +20,14 @@ export default {
   plugins: [
     scss({ include: /\.scss$/, sass: dartSass }),
     esbuild({
-      include: /\.[jt]s$/,
+      include: /\.[jt]sx?$/, // default, inferred from `loaders` option
       minify: process.env.NODE_ENV === 'production',
-      target: 'es2015' 
+      target: 'es2015' ,
+      jsx: 'transform',
+      loaders: {
+        // Enable JSX in .js files too
+        '.js': 'jsx',
+      },
     }),
     vue({
       include: /\.vue$/,
